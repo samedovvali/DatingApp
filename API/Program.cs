a6a77a5dcf3870ed088f6fcd484497609088f0c6
Ali Samadov <samadovbusiness@gmail.com>
2025-02-16 17:23:21 -0500

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,13 +10,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+// To be used in order to give permission to use to local host and share informations between each other
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
