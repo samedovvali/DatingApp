9b7882452375f83d54ed05d4b47d8aa2ca4849c2
Ali Samadov <samadovbusiness@gmail.com>
2025-02-17 22:22:50 -0500

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,5 +6,8 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; } // first way of encryption 
+    public required byte[] PasswordSalt { get; set; } // further way of encryption
+
 
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,11 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
@@ -20,6 +15,10 @@ var app = builder.Build();
 
 // To be used in order to give permission to use to local host and share informations between each other
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
